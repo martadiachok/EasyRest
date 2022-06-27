4571509baf28855e7bc660d71e357b60f277f1bd
Marta <martadiachok111@gmail.com>
2022-06-27 15:35:42 +0300

    Update LoginTest.java

--- src/test/java/com/easyrest/tests/LoginTest.java
+++ src/test/java/com/easyrest/tests/LoginTest.java
@@ -9,38 +9,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import readProperties.ConfigProvider;
 
 import java.lang.*;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
 
     @Test
     public void testLogin() throws InterruptedException {
 
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("http://localhost:3000/log-in");
+        String clientEmail = "martad4756170@gmail.com"; //ConfigProvider.clientEmail;
+        String clientPassword = "12345678"; //ConfigProvider.clientPassword;
+
+        driver.get("http://localhost:8880/log-in");
 
         WebElement inputEmail = driver.findElement(By.xpath("/html/body/div/main/div/div[2]/form/div/div[1]/div/div/input"));
         inputEmail.clear();
-        inputEmail.sendKeys("tanyasanchez@test.com");
+        inputEmail.sendKeys(clientEmail);
         Thread.sleep(1500);
 
         WebElement inputPassword = driver.findElement(By.xpath("/html/body/div/main/div/div[2]/form/div/div[2]/div/div/input"));
         inputPassword.clear();
-        inputPassword.sendKeys("1");
+        inputPassword.sendKeys(clientPassword);
         Thread.sleep(2000);
 
         WebElement buttonSignIn = driver.findElement(By.xpath("/html/body/div/main/div/div[2]/form/div/div[3]/div/button/span[1]"));
         buttonSignIn.click();
+        Thread.sleep(5000);
 
-        String URL = "http://localhost:3000/restaurants";
-        Assert.assertEquals(URL, "http://localhost:3000/restaurants" );
+        // Assert.assertEquals(ConfigProvider.urlRestaurantsMac, ConfigProvider.urlRestaurantsMac );
 
-        driver.close();
-        driver.quit();
+        String URL = "http://localhost:8880/restaurants";
+        Assert.assertEquals(URL, "http://localhost:8880/restaurants" );
 
     }
 
