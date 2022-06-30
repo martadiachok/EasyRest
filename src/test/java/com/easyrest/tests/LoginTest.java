14818ecfe0098b2d8047ba8f40f2934ca3f2b5b7
Vadym <vatsupwork@gmail.com>
2022-06-30 13:25:49 +0300

    Remove enum locators

--- src/test/java/com/easyrest/tests/LoginTest.java
+++ src/test/java/com/easyrest/tests/LoginTest.java
@@ -1,14 +1,12 @@
 package com.easyrest.tests;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.easyrest.config.ConfigProvider;
-import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.lang.*;
-import static com.easyrest.locators.LoginPageLocators.*;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class LoginTest extends BaseTest {
     @Test
@@ -21,21 +19,21 @@ public class LoginTest extends BaseTest {
         String clientEmail = ConfigProvider.clientEmail;
         String clientPassword = ConfigProvider.clientPassword;
 
-        driver.get(ConfigProvider.urlLoginMac);
+        driver.get(ConfigProvider.urlLoginWind);
 
-        WebElement inputEmail = driver.findElement(INPUT_EMAIL_FIELD.getPath());
+        WebElement inputEmail = driver.findElement(By.xpath("/html/body/div/main/div/div[2]/form/div/div[1]/div/div/input"));
         inputEmail.clear();
         inputEmail.sendKeys(clientEmail);
         logger.info("Email has been entered.");
         Thread.sleep(1500);
 
-        WebElement inputPassword = driver.findElement(INPUT_PASSWORD_FIELD.getPath());
+        WebElement inputPassword = driver.findElement(By.xpath("/html/body/div/main/div/div[2]/form/div/div[2]/div/div/input"));
         inputPassword.clear();
         inputPassword.sendKeys(clientPassword);
         logger.info("Password has been entered.");
         Thread.sleep(2000);
 
-        WebElement buttonSignIn = driver.findElement(BUTTON_SING_IN.getPath());
+        WebElement buttonSignIn = driver.findElement(By.xpath("/html/body/div/main/div/div[2]/form/div/div[3]/div/button/span[1]"));
         buttonSignIn.click();
         logger.info("SignIn button has been clicked.");
         Thread.sleep(5000);
