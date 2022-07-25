af5b7fd5464e1705740d76a02f947cfd0e97dbbe
Vadym <vatsupwork@gmail.com>
2022-07-25 10:58:32 +0300

    Add WaiterPage locators

---
+++ src/main/java/com/easyrest/pages/WaiterPage.java
@@ -0,0 +1,19 @@
+package com.easyrest.pages;
+
+import org.openqa.selenium.By;
+
+public class WaiterPage {
+
+    private By userMenu = By.xpath("//div[contains(@class,'UserMenu-root')]//button");
+    private By waiterPanel = By.xpath("//ul/a");
+
+    private By allClientsTab = By.xpath("//span[contains(.,'All')]");
+    private By assignedWaitersTab = By.xpath("//span[contains(.,'Assigned waiter')]");
+    private By historyTab = By.xpath("//span[contains(.,'History')]");
+
+    private By expandClient = By.xpath("//button[contains(@aria-label,'Show more')]");
+    private By startOrder = By.xpath("//span[contains(.,'Start order')]");
+    private By closetOrder = By.xpath("//span[contains(.,'Close order')]");
+    private By expandHistory = By.xpath("//button[contains(@aria-label,'Show more')]");
+
+}
