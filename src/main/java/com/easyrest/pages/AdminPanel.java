886396ce8a17e6d45fe40ba94e3bb88173f2e847
Marta <martadiachok111@gmail.com>
2022-07-27 17:52:50 +0300

    temp commit

--- src/main/java/com/easyrest/pages/AdminPanel.java
+++ src/main/java/com/easyrest/pages/AdminPanel.java
@@ -3,8 +3,8 @@ package com.easyrest.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import java.util.List;
+import com.easyrest.components.Calendar;
 
 public class AdminPanel extends BasePage {
 
@@ -22,87 +22,243 @@ public class AdminPanel extends BasePage {
     private By activeButton = By.xpath("//span[contains(text(),'Active')]");
     private By bannedButton = By.xpath("//span[contains(text(),'Banned')]");
     private By actionButton = By.cssSelector("td:nth-child(6)");
-    private By userName = By.cssSelector("th:nth-child(1)");
+    private By usersName = By.cssSelector("th:nth-child(1)");
     private By addModeratorButton = By.xpath("//span[text()='Add moderator']");
 
-    // Login as an admin method ???
+    /**
+     *  "CREATE MODERATOR ACCOUNT" FORM LOCATORS
+     */
+    private By nameField = By.xpath("//input[@name='name']");
+    private By emailField = By.xpath("//input[@name='email']");
+    private By phoneNumberField = By.xpath("//input[@name='phoneNumber']");
+    private By birthDateField = By.xpath("//input[@name='birthDate']");
+    private By passwordField = By.xpath("//input[@name='password']");
+    private By confirmPasswordField = By.xpath("//input[@name='repeated_password']");
+    private By createAccountButtonField = By.xpath("//span[text()='Create account']");
+    private By cancelButton = By.xpath("//span[text()='Cancel']");
 
     /**
-     * Methods to deal with users
+     * General methods to deal with all type of users.
+     * Client - any type of user role (owner or administrator or admin or waiter or moderator or user).
      */
-    public AdminPanel clickAllUsers(){
-        driver.findElement(usersButton).click();
+    public void clickAllButton(){
         driver.findElement(allButton).click();
-        return this;
     }
 
-    public AdminPanel clickActiveUsers(){
-        driver.findElement(usersButton).click();
+    public void clickActiveButton(){
         driver.findElement(activeButton).click();
-        return this;
     }
 
-    public AdminPanel clickBannedUsers(){
-        driver.findElement(usersButton).click();
+    public void clickBannedButton(){
         driver.findElement(bannedButton).click();
+    }
+
+    public String getFirstNameFromClient() {
+        List<WebElement> clientList = driver.findElements(usersName);
+        WebElement firstNameFromList = clientList.get(0);
+        return firstNameFromList.getText();
+    }
+
+    public AdminPanel clickToMakeFirstClientActive(){
+        clickBannedButton();
+        List<WebElement> userList = driver.findElements(actionButton);
+        if(userList.size() != 0) {
+            userList.get(0).click();
+        } else {
+            System.out.println("There is no one banned users.");
+        }
         return this;
     }
 
-    public AdminPanel banFromActiveUser(){
-        clickActiveUsers();
-        List<WebElement> activeUsers = driver.findElements(actionButton);
-        if(activeUsers.size() != 0) {
-            activeUsers.get(0).click();
+    public AdminPanel clickToBanFirstClient(){
+        clickActiveButton();
+        List<WebElement> userList = driver.findElements(actionButton);
+        if(userList.size() != 0) {
+            userList.get(0).click();
         } else {
             System.out.println("There is no one active users.");
         }
         return this;
     }
 
-    public WebElement checkActiveUserName() {
-        clickActiveUsers();
-        List<WebElement> activeUserName = driver.findElements(userName);
-        WebElement firstNameFromList = activeUserName.get(0);
-        firstNameFromList.getText();
-        return firstNameFromList;
-    }
-
-    public boolean checkIfActiveUserMovedToBan(WebElement firstNameFromList) {
-        clickBannedUsers();
-        List<WebElement> bannedUsersName = driver.findElements(userName);
-        boolean isUserInBannedList = true;
-        for (WebElement bannedUsersNames : bannedUsersName) {
-            if(bannedUsersNames == firstNameFromList){
+    public boolean checkIfClientMovedToBannedList(String name) {
+        clickBannedButton();
+        List<WebElement> bannedUsersName = driver.findElements(usersName);
+        for (WebElement item : bannedUsersName) {
+            if(item.getText() == name){
+                return true;
             }
         }
-        return isUserInBannedList;
+        return false;
     }
 
-    public AdminPanel removeBanFromBannedUser(){
-        clickBannedUsers();
-        List<WebElement> bannedUsers = driver.findElements(actionButton);
-        if(bannedUsers.size() != 0) {
-            bannedUsers.get(0).click();
-        } else {
-            System.out.println("There is no one banned users.");
+    public boolean checkIfClientMovedToActiveList(String name) {
+        clickActiveButton();
+        List<WebElement> activeUsersName = driver.findElements(usersName);
+        for (WebElement item : activeUsersName) {
+            if(item.getText() == name){
+                return true;
+            }
         }
+        return false;
+    }
+
+    /**
+     * Methods to deal with users
+     */
+    public AdminPanel clickUsers(){
+        driver.findElement(usersButton).click();
         return this;
     }
 
-        /*
-    public AdminPanel removeBan_checkActiveUserMovedToBan(){
-        clickBannedUsers();
-        List<WebElement> userName = driver.findElements(usersButton);
-        List<WebElement> action = driver.findElements(actionButton);
-        LinkedHashMap<List<WebElement>, List<WebElement>> bannedUsers = new LinkedHashMap<>();
-        bannedUsers.put(userName, action);
-
-        if(bannedUsers.isEmpty()) {
-            System.out.println("There is no one banned users.");
-        } else {
-            action.get(0).click();
-        }
+    public AdminPanel clickActiveUsers(){
+        clickUsers();
+        clickActiveButton();
         return this;
-    } */
+    }
+
+    public AdminPanel clickBannedUsers(){
+        clickUsers();
+        clickBannedButton();
+        return this;
+    }
+
+    public AdminPanel banUser(){
+        clickActiveUsers();
+        String userName = getFirstNameFromClient();
+        clickToBanFirstClient();
+        checkIfClientMovedToBannedList(userName);
+
+        return this;
+    }
+
+    public AdminPanel makeUserActive(){
+        clickBannedUsers();
+        String userName = getFirstNameFromClient();
+        clickToMakeFirstClientActive();
+        checkIfClientMovedToActiveList(userName);
+
+        return this;
+    }
+
+    /**
+     * Methods to deal with owners
+     */
+    public AdminPanel clickOwners(){
+        driver.findElement(ownersButton).click();
+        return this;
+    }
+
+    public AdminPanel clickActiveOwners(){
+        clickOwners();
+        clickActiveButton();
+        return this;
+    }
+
+    public AdminPanel clickBannedOwners() {
+        clickOwners();
+        clickBannedButton();
+        return this;
+    }
+
+    public AdminPanel banOwners(){
+        clickActiveOwners();
+        String userName = getFirstNameFromClient();
+        clickToBanFirstClient();
+        checkIfClientMovedToBannedList(userName);
+
+        return this;
+    }
+
+    public AdminPanel makeOwnersActive(){
+        clickBannedOwners();
+        String userName = getFirstNameFromClient();
+        clickToMakeFirstClientActive();
+        checkIfClientMovedToActiveList(userName);
+
+        return this;
+    }
+
+    /**
+     * Methods to deal with moderators
+     */
+    public AdminPanel clickModerators(){
+        driver.findElement(moderatorsButton).click();
+        return this;
+    }
+
+    public AdminPanel clickActiveModerators(){
+        clickModerators();
+        clickActiveButton();
+        return this;
+    }
+
+    public AdminPanel clickBannedModerators(){
+        clickModerators();
+        clickBannedButton();
+        return this;
+    }
+
+    public AdminPanel banModerator(){
+        clickActiveModerators();
+        String userName = getFirstNameFromClient();
+        clickToBanFirstClient();
+        checkIfClientMovedToBannedList(userName);
+
+        return this;
+    }
+
+    public AdminPanel makeModeratorActive(){
+        clickBannedModerators();
+        String userName = getFirstNameFromClient();
+        clickToMakeFirstClientActive();
+        checkIfClientMovedToActiveList(userName);
+
+        return this;
+    }
+
+    public AdminPanel clickAddModeratorButton(){
+        clickModerators();
+        driver.findElement(addModeratorButton).click();
+        return this;
+    }
+
+    /**
+     *  Methods to fill form "CREATE MODERATOR ACCOUNT"
+     */
+    public AdminPanel inputName (String name) {
+        driver.findElement(nameField).sendKeys(name);
+        return this;
+    }
+
+    public AdminPanel inputEmail (String email) {
+        driver.findElement(emailField).sendKeys(email);
+        return this;
+    }
+
+    public AdminPanel inputPhone(String phoneNumber) {
+        driver.findElement(phoneNumberField).sendKeys(phoneNumber);
+        return this;
+    }
+
+    public AdminPanel inputPassword (String password) {
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(confirmPasswordField).sendKeys(password);
+        return this;
+    }
+
+    public AdminPanel clickOnBirthdayField() {
+        driver.findElement(birthDateField).click();
+        return new Calendar(driver);
+    }
+
+
+
+
+    /**
+     * Methods to deal with restaurants
+     */
+
+
 
 }
