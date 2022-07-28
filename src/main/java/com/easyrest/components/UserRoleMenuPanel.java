c73ae59a6eb2bf348a72ad9fa55759a1410a67ee
Marta <martadiachok111@gmail.com>
2022-07-28 13:08:54 +0300

    Add user panel component; update URL

--- src/main/java/com/easyrest/components/UserRoleMenuPanel.java
+++ src/main/java/com/easyrest/components/UserRoleMenuPanel.java
@@ -4,20 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class UserRoleMenuPanel {
-    public UserRoleMenuPanel(WebDriver driver) {
-        this.driver = driver;
-    }
-
     protected WebDriver driver;
 
-    private By userPanelButton = By.xpath("//ul[@role='menu']//child::a");
-    private By logOutButton = By.xpath("//ul[@role='menu']//child::li");
+    private final By userPanelButton = By.xpath("//ul[@role='menu']//child::a");
+    private final By logOutButton = By.xpath("//ul[@role='menu']//child::li");
+    private final By userProfileIcon = By.xpath("//button[@aria-haspopup='true']//child::div");
+
+    public UserRoleMenuPanel(WebDriver driver) { this.driver = driver; }
+
+    public UserRoleMenuPanel clickOnUserProfileIcon(){
+        driver.findElement(userProfileIcon).click();
+        return new UserRoleMenuPanel(driver);
+    }
 
     public void clickOnUserPanel() {
+        clickOnUserProfileIcon();
         driver.findElement(userPanelButton).click();
     }
 
     public void clickOnLogOut() {
+        clickOnUserProfileIcon();
         driver.findElement(logOutButton).click();
     }
 
