8fb66301b58b87c3b25ca7fda127cbee161803b2
Marta <martadiachok111@gmail.com>
2022-08-02 15:28:26 +0300

    temp commit

---
+++ src/main/java/com/easyrest/components/Header.java
@@ -0,0 +1,33 @@
+package com.easyrest.components;
+
+import com.easyrest.pages.HomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Header {
+    private final By logo = By.xpath("//a[text()='Easy-rest']");
+    private final By homeButton = By.xpath("//a[@aria-current='page']");
+    private final By restaurantListButton = By.xpath("//header[contains(@class,'MuiAppBar-root')]/div/nav/a[2]");
+    private final By userProfileIcon = By.xpath("//button[@aria-haspopup='true']//child::div");
+
+    protected WebDriver driver;
+    public Header(WebDriver driver) { this.driver = driver; }
+
+    protected void clickOnUserProfileIcon(){
+        driver.findElement(userProfileIcon).click();
+    }
+
+    private void clickOnRestaurantList(){
+        driver.findElement(restaurantListButton);
+    }
+
+    private void clickOnHome(){
+        driver.findElement(homeButton);
+    }
+
+    private void clickOnLogo(){
+        driver.findElement(logo);
+    }
+
+}
--- src/main/java/com/easyrest/components/UserRoleMenuPanel.java
+++ src/main/java/com/easyrest/components/UserRoleMenuPanel.java
@@ -2,28 +2,25 @@ package com.easyrest.components;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import com.easyrest.components.Header;
 
 public class UserRoleMenuPanel {
     protected WebDriver driver;
 
     private final By userPanelButton = By.xpath("//ul[@role='menu']//child::a");
     private final By logOutButton = By.xpath("//ul[@role='menu']//child::li");
-    private final By userProfileIcon = By.xpath("//button[@aria-haspopup='true']//child::div");
+
+    Header header = new Header(driver);
 
     public UserRoleMenuPanel(WebDriver driver) { this.driver = driver; }
 
-    private UserRoleMenuPanel clickOnUserProfileIcon(){
-        driver.findElement(userProfileIcon).click();
-        return new UserRoleMenuPanel(driver);
-    }
-
     public void clickOnUserPanel() {
-        clickOnUserProfileIcon();
+        header.clickOnUserProfileIcon();
         driver.findElement(userPanelButton).click();
     }
 
     public void clickOnLogOut() {
-        clickOnUserProfileIcon();
+        header.clickOnUserProfileIcon();
         driver.findElement(logOutButton).click();
     }
 
