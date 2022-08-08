29ee1bc4a9ba183d3a60abdbde85d90ce6c0c9a6
Marta <martadiachok111@gmail.com>
2022-08-08 13:17:19 +0300

    code cleanUp

--- src/main/java/com/easyrest/components/HeaderForAuthorizedUser.java
+++ src/main/java/com/easyrest/components/HeaderForAuthorizedUser.java
@@ -4,12 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HeaderForAuthorizedUser {
+    private WebDriver driver;
+    
     private final By logo = By.xpath("//a[text()='Easy-rest']");
     private final By homeButton = By.xpath("//a[@aria-current='page']");
     private final By restaurantListButton = By.xpath("//header[contains(@class,'MuiAppBar-root')]/div/nav/a[2]");
     private final By userProfileIcon = By.xpath("//button[@aria-haspopup='true']//child::div");
 
-    private WebDriver driver;
     public HeaderForAuthorizedUser(WebDriver driver) { this.driver = driver; }
 
     public UserRoleMenuPanel clickOnUserProfileIcon(){
