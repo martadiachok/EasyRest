e759e2f4b308c533d428b986c08fba4e9d1fc261
Vadym <vatsupwork@gmail.com>
2022-08-10 21:01:54 +0300

    Add waiter pages

---
+++ src/main/java/com/easyrest/pages/waiterPanel/AllOrders.java
@@ -0,0 +1,11 @@
+package com.easyrest.pages.waiterPanel;
+
+import com.easyrest.pages.WaiterPage;
+import org.openqa.selenium.WebDriver;
+
+public class AllOrders extends WaiterPage {
+
+    public AllOrders(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/main/java/com/easyrest/pages/waiterPanel/OrdersHistory.java
@@ -0,0 +1,11 @@
+package com.easyrest.pages.waiterPanel;
+
+import com.easyrest.pages.WaiterPage;
+import org.openqa.selenium.WebDriver;
+
+public class OrdersHistory extends WaiterPage {
+
+    public OrdersHistory(WebDriver driver) {
+        super(driver);
+    }
+}
