c795963036e0e3a3738eaebfe52debbca5901727
Marta <martadiachok111@gmail.com>
2022-08-18 11:28:55 +0300

    temp commit

--- src/test/java/com/easyrest/tests/SignUpTest.java
+++ src/test/java/com/easyrest/tests/SignUpTest.java
@@ -2,11 +2,14 @@ package com.easyrest.tests;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.easyrest.config.ConfigProvider;
+import com.easyrest.constants.Constants;
 import com.easyrest.facade.SignInFacade;
 import com.easyrest.pages.SignInPage;
 import com.easyrest.pages.SignUpPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class SignUpTest extends BaseTest {
 
@@ -23,7 +26,7 @@ public class SignUpTest extends BaseTest {
     private SignInFacade signInFacade;
 
     @Test (priority = 1)
-    public void signUpPositiveTest() throws InterruptedException {
+    public void signUpPositiveTest() {
         signUpPage = new SignUpPage(driver);
         extent = new ExtentReports();
         test = extent.createTest("signUpPositiveTest");
@@ -35,13 +38,14 @@ public class SignUpTest extends BaseTest {
         signUpPage.clickOnBirthdayField().inputDate(birthYear, birthMonth, birthDate);
         signUpPage.inputPassword(password);
         signUpPage.clickCreateAccount();
-
-        String currentUrl = driver.driver.getCurrentUrl();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        String currentUrl = driver.getCurrentUrl();
         Assert.assertEquals(currentUrl, ConfigProvider.signInPageUrl, "Sign up failed. Sign in page was not reached.");
     }
 
     @Test (priority = 2)
-    public void secondCheckSignUp_tryToSignIn() throws InterruptedException {
+    public void secondCheckSignUp_tryToSignIn() {
         signInPage = new SignInPage(driver);
         signInFacade = new SignInFacade(driver);
         extent = new ExtentReports();
@@ -50,7 +54,8 @@ public class SignUpTest extends BaseTest {
         signInPage.goToSignInPage();
         signInFacade.signIn(email, password);
 
-        String currentUrl = driver.driver.getCurrentUrl();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Constants.TimeoutVariable.IMPLICIT_WAIT));
+        String currentUrl = driver.getCurrentUrl();
         Assert.assertEquals(currentUrl, ConfigProvider.restaurantsPageUrl, "Sign in of new user was not successful. Restaurant page was not reached.");
     }
 
