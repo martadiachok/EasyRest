7c21f7236839a0a8716ac1c4aaf229b7c27a0489
Vadym <vatsupwork@gmail.com>
2022-08-23 22:42:06 +0300

    Remove logout from AdministratorSignInTest

--- src/test/java/com/easyrest/tests/administrator/AdministratorSignInTest.java
+++ src/test/java/com/easyrest/tests/administrator/AdministratorSignInTest.java
@@ -2,7 +2,6 @@ package com.easyrest.tests.administrator;
 
 import com.easyrest.components.SingInAlert;
 import com.easyrest.config.ConfigProvider;
-import com.easyrest.facade.AuthorizedHeaderMenuPanelFacade;
 import com.easyrest.facade.SignInFacade;
 import com.easyrest.pages.AdministratorPage;
 import com.easyrest.pages.SignInPage;
@@ -19,6 +18,7 @@ public class AdministratorSignInTest extends BaseTest {
     private final String password = ConfigProvider.administratorPassword;
     private SignInFacade signInFacade;
 
+
     @BeforeMethod
     public void goToSignInPage() {
         SignInPage signInPage = new SignInPage(driver);
@@ -28,12 +28,10 @@ public class AdministratorSignInTest extends BaseTest {
 
     @Test
     public void validSignIn() {
-        signInFacade.signIn(email, password);
         AdministratorPage administratorPage = new AdministratorPage(driver);
+        signInFacade.signIn(email, password);
         administratorPage.waitForPagePresence();
         String currentUrl = driver.getCurrentUrl();
-        AuthorizedHeaderMenuPanelFacade userMenu = new AuthorizedHeaderMenuPanelFacade(driver);
-        userMenu.clickOnLogoutMenuItem();
 
         Assert.assertEquals(currentUrl, administratorPanelUrl, "The current url doesn't match the expected one.");
     }
@@ -49,8 +47,8 @@ public class AdministratorSignInTest extends BaseTest {
 
     @Test(dataProvider = "fake")
     public void fakeSignIn(String email, String password) {
-        signInFacade.signIn(email, password);
         SingInAlert alert = new SingInAlert(driver);
+        signInFacade.signIn(email, password);
         String actualAlertMessage = alert.showMessage();
 
         Assert.assertEquals(actualAlertMessage, expectedAlertMessage, "The alert message doesn't match the expected one.");
