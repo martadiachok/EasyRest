627c155b3c1c1707b85f17d82e6063306ea39702
Marta <martadiachok111@gmail.com>
2022-08-25 10:36:16 +0300

    code cleanUp

--- src/test/java/com/easyrest/tests/Listeners.java
+++ src/test/java/com/easyrest/tests/Listeners.java
@@ -14,18 +14,18 @@ public class Listeners extends BaseTest implements ITestListener {
     }
 
    public void onTestSuccess(ITestResult result) {
-       System.out.println("Test passed: " + result.getMethod().getMethodName());
+       test.pass(result.getMethod().getMethodName());
     }
 
     public void onTestFailure(ITestResult result) {
-       System.out.println("Test was failed: " + result.getMethod().getMethodName());
-       String testMethodName = result.getMethod().getMethodName();
+        test.fail(result.getMethod().getMethodName());
+        String testMethodName = result.getMethod().getMethodName();
         try {
             BaseTest instance = (BaseTest)result.getInstance();
             driver = instance.getDriver();
-        } catch (Exception e) {
+         } catch (Exception e) {
             System.out.println(e);
-        }
+         }
 
         try {
             getScreenShotPath(testMethodName);
@@ -35,7 +35,7 @@ public class Listeners extends BaseTest implements ITestListener {
     }
 
     public void onTestSkipped(ITestResult result) {
-        System.out.println("Test was skipped: " + result.getMethod().getMethodName());
+        test.skip(result.getMethod().getMethodName());
     }
 
 }
