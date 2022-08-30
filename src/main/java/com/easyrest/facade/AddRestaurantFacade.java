3437dbca49ad9ab21b149ffd830f1d20a7fef95b
= <=>
2022-08-30 20:17:55 +0300

    Add AddRestaurantFacade and SignInTest for customer

---
+++ src/main/java/com/easyrest/facade/AddRestaurantFacade.java
@@ -0,0 +1,28 @@
+package com.easyrest.facade;
+
+import com.easyrest.components.AddRestaurant;
+import com.easyrest.components.customer.Right_sideMenu;
+import com.easyrest.pages.customerPanel.MyRestaurants;
+import org.openqa.selenium.WebDriver;
+
+public class AddRestaurantFacade {
+    private WebDriver driver;
+    AddRestaurant addRestaurant;
+    Right_sideMenu right_sideMenu;
+    MyRestaurants myRestaurants;
+
+    public AddRestaurantFacade(WebDriver driver) {
+        this.driver = driver;
+        addRestaurant = new AddRestaurant(driver);
+        right_sideMenu = new Right_sideMenu(driver);
+        myRestaurants = new MyRestaurants(driver);
+    }
+
+    public void addRestaurant(String name, String address){
+        right_sideMenu.seeMyRestaurants()
+                .clickAddRestaurantPlusButton()
+                .enterRestaurantName(name).enterRestaurantAddress(address)
+                .approveRestaurantAdding();
+    }
+
+}
