4927609bac509f84258c0b5d00eb2b4cf1dcc34e
= <=>
2022-09-01 15:02:46 +0300

    resolve conflict

--- src/test/java/com/easyrest/userData/FakeData.java
+++ src/test/java/com/easyrest/userData/FakeData.java
@@ -5,27 +5,11 @@ import com.github.javafaker.Faker;
 public class FakeData {
     private static Faker faker = new Faker();
 
-    public static String getFullName() {
-        return faker.name().fullName();
-    }
+    public static String getFullName() { return faker.name().fullName(); }
+    public static String getEmail() { return faker.internet().emailAddress(); }
+    public static String getPassword() { return faker.internet().password(8, 12); }
+    public static String getPhone() { return faker.phoneNumber().phoneNumber(); }
 
-    public static String getEmail() {
-        return faker.internet().emailAddress();
-    }
-
-    public static String getPassword() {
-        return faker.internet().password(8, 12);
-    }
-
-    public static String getPhone() {
-        return faker.phoneNumber().phoneNumber();
-    }
-
-    public static String getRestaurantName() {
-        return faker.funnyName().name();
-    }
-
-    public static String getRestaurantAddress() {
-        return faker.address().fullAddress();
-    }
+    public static String getRestaurantName(){return faker.funnyName().name();}
+    public static String getRestaurantAddress(){return faker.address().fullAddress();}
 }
